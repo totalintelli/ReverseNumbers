6a44213f51458871f5ccabb442c46e1d2b2a20f2
totalintelli <kwanwoo5@naver.com>
2015-08-11 16:13:33 +0900

    main 함수 추가

--- ReverseNumbers.c
+++ ReverseNumbers.c
@@ -14,3 +14,17 @@ int main(int argc, char *argv[]);
 void Input(Long *number, Long(*numbers));
 void ReverseNumbers(Long number, Long(*numbers));
 void Output(Long number, Long(*numbers));
+
+int main(int argc, char *argv[]) {
+	Long number;
+	Long numbers[MAX];
+
+	Input(&number, numbers);
+	ReverseNumbers(number, numbers);
+	Output(number, numbers);
+
+	return 0;
+}
+void Input(Long *number, Long(*numbers));
+void ReverseNumbers(Long number, Long(*numbers));
+void Output(Long number, Long(*numbers));
